a5fed57fc57ddc8664b786f8ca101f3afa1b7ff7
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-03-04 10:11:29 +0700

    modifikasi pemberian komentar pada prak1

--- Pertemuan4/variabel_konstanta.php
+++ Pertemuan4/variabel_konstanta.php
@@ -1,16 +1,25 @@
 <?php
-//Langkah 3
+/*
+Membuat file baru bernama variabel_konstanta.php
+*/
+/*
+Langkah 3: menambahkan kode program pada file variabel_konstanta.php
+*/
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
 echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
 
-//Langkah 5
+/*
+Langkah 5: menambahkan kode program pada file variabel_konstanta.php
+*/
 $benar = true;
 $salah = false;
 echo "Variabel benar: $benar, Variabel salah: $salah";
 
-//Langkah 6
+/*
+Langkah 6: menambahkan kode program pada file variabel_konstanta.php
+*/
 //Mendefinisikan konstanta untuk nilai tetap
 define("NAMA_SITUS", "WebsiteKu.com");
 define("TAHUN_PENDIRIAN", 2023);
