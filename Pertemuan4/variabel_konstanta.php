06668552194a324ee4c8d38c8b66a69385db115e
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-03-04 09:40:10 +0700

    pemberian komentar pada prak1

--- Pertemuan4/variabel_konstanta.php
+++ Pertemuan4/variabel_konstanta.php
@@ -1,13 +1,16 @@
 <?php
+//Langkah 3
 $angka1 = 10;
 $angka2 = 5;
 $hasil = $angka1 + $angka2;
 echo "Hasil penjumlahan $angka1 dan $angka2 adalah $hasil.";
 
+//Langkah 5
 $benar = true;
 $salah = false;
 echo "Variabel benar: $benar, Variabel salah: $salah";
 
+//Langkah 6
 //Mendefinisikan konstanta untuk nilai tetap
 define("NAMA_SITUS", "WebsiteKu.com");
 define("TAHUN_PENDIRIAN", 2023);
