7d4475fe63c33793bf291dee383e812db7b9f80d
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-03-18 21:26:10 +0700

    update variabel server

---
+++ Pertemuan6/praktik php/variabel_server.php
@@ -0,0 +1,14 @@
+<?php
+echo $_SERVER['PHP_SELF']; //menampilkan nama file berdasarkan dari dokumen
+echo "<br>";
+echo $_SERVER['SERVER_NAME']; //menampilkan nama server
+echo "<br>";
+echo $_SERVER['HTTP_HOST']; //menampilkan nama host
+echo "<br>";
+echo $_SERVER['HTTP_REFERER']; //menampilkan halaman url yang menjadi referensi
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT']; //menampilkan user yang sedang melakukan request konten
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME']; //menampilkan nama script
+echo "<br>";
+?>
