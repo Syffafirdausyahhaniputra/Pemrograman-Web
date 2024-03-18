1db5affae33e8cc1d303666982a99e44f7ca3043
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-03-18 22:41:36 +0700

    update empty

---
+++ Pertemuan7/empty.php
@@ -0,0 +1,19 @@
+<?php
+
+//prak 2 langkah 2
+$myArray = array(); //array kosong
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong";
+} else {
+    echo "Array terdefinisi dan tidak kosong";
+}
+
+echo "<hr>";
+
+//prak 2 langkah 4
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong";
+}
+?>
