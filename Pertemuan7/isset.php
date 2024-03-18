9f0b5b9800711cb04976d9eb88de76c4bbb16ed7
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-03-18 22:35:49 +0700

    update isset

---
+++ Pertemuan7/isset.php
@@ -0,0 +1,20 @@
+<?php
+
+//prak 1 langkah 3
+$umur;
+if (isset($umur) && $umur >= 18) {
+    echo "Anda sudah dewasa.";
+} else {
+    echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
+}
+
+echo "<hr>";
+
+//prak 1 langkah 5
+$data = array("nama" => "Jane", "usia" => 25);
+if (isset($data["nama"])) {
+    echo "Nama: ". $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array";
+}
+?>
