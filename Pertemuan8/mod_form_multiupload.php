4e5e1808b0a4b9d880ac65b5a102e3d345a2a1d9
syffafirdausyahhaniputra <syffafirdausyahhaniputra@gmail.com>
2024-03-23 13:47:40 +0700

    upload prak 2 multi upload file

---
+++ Pertemuan8/mod_form_multiupload.php
@@ -0,0 +1,14 @@
+<!-- Prak 2 Soal 2.2 -->
+<html>
+    <head>
+        <title>Multiupload Dokumen</title>
+    </head>
+    <body>
+        <h2>Unggah Dokumen</h2>
+        <form action="mod_proses_upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="files[]" multiple="multiple"
+            accept=" .jpg, .jpeg, .png, .gif">
+        <input type="submit" value="Unggah">
+    </form>
+</body>
+</html>
